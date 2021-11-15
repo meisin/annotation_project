5bcf3353ca136b7b56da06d377572439f3de0ce2
Meisin Lee <meisin123@gmail.com>
2021-11-16 04:28:18 +0800

    Revert "simplify annotation checking"
    This reverts commit 4f33db8d14eee1e7d2dc988c38302bc17c876ce3.

--- annotation_project/settings.py
+++ annotation_project/settings.py
@@ -21,12 +21,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a#uo3%+7ovr0yz$6-0eea$r6gpne!2l%sz3@v*3qs7pijv@ag-'
+#SECRET_KEY = 'a#uo3%+7ovr0yz$6-0eea$r6gpne!2l%sz3@v*3qs7pijv@ag-'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    #'0.0.0.0'
+    'commodity-annotation.herokuapp.com'
+    #'127.0.0.1'
+]
 
 
 # Application definition
@@ -49,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'annotation_project.urls'
@@ -125,5 +134,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
